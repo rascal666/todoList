f397b20c59ce89bbd3a58c93348931f4bfbbb244
Nastya <n_m_666@mail.ru>
2022-01-31 17:50:33 +0500

    логика папок

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import '../style/todoForm.sass'
+
+function TodoForm ({ getTodo, setTodo }) {
+
+    function keyDown (ev) {
+        if (ev.keyCode === 13) {
+            name()
+        }
+    }
+
+    const textInput = useRef()
+    function name () {
+        if (textInput.current.value) {
+            setTodo([...getTodo, {
+                title: textInput.current.value,
+                id: uuidv4(),
+                status: true
+            }])
+        }
+        textInput.current.value = ''
+    }
+    return (
+        <div className="form" onKeyDown={keyDown}>
+            <input className="form__input" ref={textInput} />
+            <div className="form__button"
+                onClick={name}
+
+                type="">add task</div>
+        </div>
+    )
+}
+
+
+export default TodoForm
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
-import WebFont from 'webfontloader';
 
-WebFont.load({
-
-  google: {
-    families: ['Droid Sans', 'Droid Serif']
-
-  }
-});
 
 ReactDOM.render(
   <App />,
